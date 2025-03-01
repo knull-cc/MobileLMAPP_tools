ea0eccbac9679300f531afea4c0e36f9737dee13
knull-cc <knullcc@gmail.com>
2025-03-01 12:09:11 +0800

    first commit

---
+++ finetune/pass_lora_finetune_ocnli_first_absolution_path.sh
@@ -0,0 +1,29 @@
+formatted_time=$(date +"%Y%m%d%H%M%S")
+echo $formatted_time
+
+export CUDA_VISIBLE_DEVICES=0
+
+deepspeed --include localhost:0 --master_port 19888 /content/MiniCPM-MobileLMAPP/finetune/finetune.py \
+    --model_name_or_path /content/MiniCPM-2B-sft-bf16 \
+    --output_dir output/OCNLILoRA/$formatted_time/ \
+    --train_data_path /content/MiniCPM-MobileLMAPP/finetune/data/MobileLMAPP/train.json \
+    --eval_data_path /content/MiniCPM-MobileLMAPP/finetune/data/MobileLMAPP/dev.json \
+    --learning_rate 5e-5 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --model_max_length 1024 \
+    --fp16 \
+    --use_lora \
+    --gradient_accumulation_steps 4 \
+    --warmup_steps 100 \
+    --max_steps 1000 \
+    --weight_decay 0.01 \
+    --evaluation_strategy steps \
+    --eval_steps 500 \
+    --save_strategy steps \
+    --save_steps 500 \
+    --seed 42 \
+    --log_level info \
+    --logging_strategy steps \
+    --logging_steps 10 \
+    --deepspeed /content/MiniCPM-MobileLMAPP/finetune/configs/ds_config_zero2_offload.json
