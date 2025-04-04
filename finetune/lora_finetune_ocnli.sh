dad91c64f218d0b574d8456c03a1fbf82419c270
knull-cc <knullcc@gmail.com>
2025-04-04 11:54:24 +0800

    update lora.sh

--- finetune/lora_finetune_ocnli.sh
+++ finetune/lora_finetune_ocnli.sh
@@ -7,12 +7,12 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 deepspeed --include localhost:0 --master_port 19888 finetune.py \
     --model_name_or_path /content/MiniCPM3-4B \
     --output_dir output/OCNLILoRA/$formatted_time/ \
-    --train_data_path data/data_3765/train.json \
-    --eval_data_path data/data_3765/dev.json \
+    --train_data_path data/data_18k/train.json \
+    --eval_data_path data/data_18k/dev.json \
     --learning_rate 2e-5 \
     --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 2 \
-    --model_max_length 1024 \
+    --model_max_length 512 \
     --fp16 \
     --use_lora \
     --gradient_accumulation_steps 1 \
